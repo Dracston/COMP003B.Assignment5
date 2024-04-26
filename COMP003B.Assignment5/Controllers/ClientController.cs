64f983a9ca08c057499f201dcf902cd82974761b
Logan Jones <lpj11101@gmail.com>
2024-04-26 00:02:50 -0700

    Add project files.

---
+++ COMP003B.Assignment5/Controllers/ClientController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using COMP003B.Assignment5.Models;
+
+namespace COMP003B.Assignment5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class ClientController : Controller
+    {
+        //Create new private list
+        private List<Client> _clients = new List<Client>();
+
+        //Add pre-filled options to the list
+        public ClientController()
+        {
+            _clients.Add(new Client { });
+        }
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP003B.Assignment5/Models/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public string Material {  get; set; }
+        public int Size { get; set; }
+        public string Engraving {  get; set; }
+
+        public string Gender { get; set; }
+    }
+}
